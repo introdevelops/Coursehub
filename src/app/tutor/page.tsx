b5e4ca303db674da071d18fc8b995b264213ae0a
Neeraj23 <neerajbis2326@gmail.com>
2025-01-01 23:11:09 +0530

    updated

--- src/app/tutor/page.tsx
+++ src/app/tutor/page.tsx
@@ -20,7 +20,7 @@ const TutorLandingPage = () => {
    {(!(status==="loading"))?(<div
       className="relative h-screen flex items-center justify-center bg-cover bg-center"
       style={{
-        backgroundImage: "url('/tutor_landing.jpeg')",
+        backgroundImage: "url('/tutor_landing.jpg')",
        
       }}
     >
